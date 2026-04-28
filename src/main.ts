417651a37b4205e3d970d7aa0b8a4a9b0b18f7b5
Gino Calderoni <ginocalderoni04@gmail.com>
2026-04-28 16:46:12 -0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Allow the React frontend (running on a different port in dev) to call this API
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3001'],
+ app.enableCors({
+    origin: '*',
     methods: ['GET', 'POST'],
   });
 
